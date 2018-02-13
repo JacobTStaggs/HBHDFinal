6ae34c8066439ce510d27629da6a1bfd4497e504
Jacob Staggs <staggs11@marshall.edu>
2018-02-13 09:36:46 -0500

    changed port'

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000);
+app.listen(8080);
 
-console.log("Running at Port 3000");
+console.log("Running at Port 8080");
