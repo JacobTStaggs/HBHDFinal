111b00d8b734bbf46cf94b6c437562f19a5278c2
Jacob Staggs <staggs11@marshall.edu>
2018-02-13 10:35:02 -0500

    changed a small issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
-var port = normalizePort(process.env.PORT || 8080)
+var port =  8080;
 app.use(express.static(__dirname+'/public'));
 
 app.get('/',function(req,res){
